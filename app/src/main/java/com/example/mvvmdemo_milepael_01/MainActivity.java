f2959bbece0bf4861db60f4c166eb3165fd25a0a
patrickmylius <patrickmylius@outlook.dk>
2021-02-01 15:15:29 +0100

    Added Milepæl 2

--- app/src/main/java/com/example/mvvmdemo_milepael_01/MainActivity.java
+++ app/src/main/java/com/example/mvvmdemo_milepael_01/MainActivity.java
@@ -2,13 +2,38 @@ package com.example.mvvmdemo_milepael_01;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    //Global vars
+    Button changeButton, resetButton;
+    TextView textView;
+    EditText editText;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        changeButton = (Button) findViewById(R.id.changeButton);//Change button
+        editText = (EditText) findViewById(R.id.editText); //EditText, Takes user input
+        textView = (TextView) findViewById(R.id.textView); //Textview
+
+        //changeButton on click listener
+        changeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String userInput = editText.getText().toString(); //Converts editext to toString
+                textView.setText(userInput); //Sets Textview to be editText input
+
+
+            }
+        });
     }
 }
